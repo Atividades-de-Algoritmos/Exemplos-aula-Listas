350af1ca6c9ddc0427540589e27666ac2a738358
Carlos <lopes.carlos@academico.ifpb.edu.br>
2022-07-08 16:52:18 -0300

    Média com listas ➗➕

--- ex03.py
+++ ex03.py
@@ -3,24 +3,28 @@
 # Michel Silva
 # Emanuel Frank
 # Carlos Eduardo
-# data: 26/06/2022
 #
+# data: 08/07/2022
 #
 # 3 – ler uma lista de 4 notas e em seguida mostra
 # as notas e a média.
 
-# entrada de dados
-lista = [] # cria uma lista vazia para armazenar os dados
-soma = 0 # cria uma variável para armazenar a soma das notas
+# Processamento e entrada de dados
 
-for i in range(4): # para cada i em 0 a 3 faça o seguinte
-    lista.append(float(input(f"Digite uma nota[{i+1}]: "))) # adiciona a nota digitada na lista e incrementa o contador
-    soma += lista[i] # soma a nota digitada na variável soma
+lista = [] # Criando uma lista vazia para armazenar os dados
+soma = 0 # Criando uma variável para armazenar a soma das notas
 
-# saida de dados
-print(f"As notas são: {lista}") # imprime a lista de notas
-print(f"A média é: {soma/4}") # imprime a média das notas
+for i in range(4): # Para cada 'i' -> item no 'range(4)' tamanho de 0 a 3 faça o seguinte
+    lista.append(float(input(f"Digite uma nota[{i+1}]: "))) # Adicione a nota digitada no fim da lista com .append()
+    soma += lista[i] # Soma a nota digitada na variável soma
+
+# Saída de dados
+
+print(f"\nAs notas são: {lista}") # Imprimindo a lista de notas
+print(f"A média é: {soma/4}") # Imprimindo a média das notas
+
 # ou
-# print(f"A média é: {sum(lista)/len(lista)}") # imprime a média das notas
 
-print("Fim do programa")
+# print(f"A média é: {sum(lista)/len(lista)}") # Imprimindo a média das notas sum() soma todos os elementos da lista e o len() conta a quantidade de elementos existentes na lista
+
+print("\nfim do programa") # Informando ao usuário que o programa terminou
